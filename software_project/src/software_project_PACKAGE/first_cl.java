291a3ffa5057f2dd42632e70572bf207045cf401
ahmed <ahmadawwadza7@Gmail.com>
2023-02-11 22:12:19 +0200

    1c

---
+++ software_project/src/software_project_PACKAGE/first_cl.java
@@ -0,0 +1,13 @@
+package software_project_PACKAGE;
+
+public class first_cl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("ahmed");
+	}
+
+	
+	
+	
+}
