f988c1e68cfa345d6c48da24f688ec5a99cf222a
Ahmed Moeen Awwad <124081794+ahmed-awwad00@users.noreply.github.com>
2023-05-13 11:40:52 +0300

    Update OrderDistributor.java

--- src/main/java/MyPackage/OrderDistributor.java
+++ src/main/java/MyPackage/OrderDistributor.java
@@ -12,7 +12,7 @@ public class OrderDistributor {
 
    
 
-	public int distributeOrders(String aorders) {
+	public int distributeOrders() {
         int totalOrders = 200;
         int numWorkers = 12;
         int avg = totalOrders / numWorkers;
@@ -22,7 +22,10 @@ public class OrderDistributor {
      
    
             ordersAssigned++;
-            return (ordersAssigned % avg == 0) ? currentWorkerIndex++:currentWorkerIndex;
+            if (ordersAssigned % avg == 0) {
+                currentWorkerIndex++;
+            }
+            return currentWorkerIndex;
            
         }
 	
