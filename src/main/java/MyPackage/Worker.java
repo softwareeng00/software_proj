7f5ac5d7b3ced0872e55e7c3a54dfe9d7373cf57
Ahmed Moeen Awwad <124081794+ahmed-awwad00@users.noreply.github.com>
2023-05-13 10:54:10 +0300

    Update Worker.java

--- src/main/java/MyPackage/Worker.java
+++ src/main/java/MyPackage/Worker.java
@@ -2,22 +2,21 @@ package mypackage;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 
 
 public class Worker {
 	
 	int id;
-	static String name;
-	 static String address;
-	 static String date;
-	 private static final Logger logger = Logger.getLogger(Worker.class.getName());
+	 String name;
+	  String address;
+	  String date;
 
-	private static int phone;
-	private static String adder;
-	private static String password;
-	static int isWorker=0;
+
+	  int phone;
+      String adder;
+	  String password;
+	 int isWorker=0;
 	
 	 static List<Worker> workerList =new ArrayList<>();
 	
@@ -25,17 +24,22 @@ public class Worker {
 	 static List<String> passWorker =new ArrayList<>() ;
 	 
 	 public Worker() {
-			Worker.name="ahmed";
-			Worker.address="Nablus";
-			Worker.phone=11;
-			Worker.date="ss";
+			this.name="ahmed";
+			this.address="Nablus";
+			this.phone=11;
+			this.date="ss";
 			emailWorker.add("awawd");
 			passWorker.add("123");
 		}
 	
 	public void creatWorker(String name,String address,int phone,String email,String password )
 	{
-
+	this.setName(name);
+	this.setAdder(address);
+	this.setPhone(phone);
+	
+	this.setEmail(email);
+	this.setPassword(password);
 	if(!workerList.contains(this))
 		workerList.add(this);
 	}
@@ -61,12 +65,12 @@ public class Worker {
 		if (!emailWorker.contains(email))
 			 emailWorker.add(email);
 	}
-	public static String getPassword()
+	public  String getPassword()
 	{
 		return password;
 	}
-	public static void setPassword(String password) {
-		Worker.password = password;
+	public void setPassword(String password) {
+		this.password = password;
 		if (!passWorker.contains(password))
 			passWorker.add(password);
 	}
@@ -94,7 +98,7 @@ public class Worker {
 	}
 	
 	public static void printWorker() {
-		logger.info("Name         " +"emmaill    \n"+
+		System.out.println("Name         " +"emmaill    \n"+
 				           "Aya Moin     " +"ayamoinn44@gmail.com\n"+
 				           "Mohmed samer " +"mohmd55t44@gmail.com\n"+
 				           "jad mohmd    " +"jadmohmd33@gmail.com\n");
@@ -102,7 +106,7 @@ public class Worker {
 	
 	public static void workerName()
 	{
-		logger.info("Name" +
+		System.out.println("Name" +
 		           "Aya Moin\n"+
 		           "Mohmed samer\n"+
 		           "jad mohmd\n\n");
@@ -110,16 +114,16 @@ public class Worker {
 	
 	
 	public static void prodinclean() {
-		logger.info("Name         " +"    Number    \n"+
+		System.out.println("Name         " +"    Number    \n"+
 		           "Canves1     " +"      1\n"+
 		           "Canves + Cover " +"   2\n"+
 		           "Cover    " +"         3\n");
 	}
 	public static void breakTime()
 	{
-		logger.info("\n Your Work Start at 8:30  to 4:00");
+		System.out.println("\n Your Work Start at 8:30  to 4:00");
 
-		logger.info("\n Your break at 11:00 to 11:30 / break at 2:00 to 2:40");
+		System.out.println("\n Your break at 11:00 to 11:30 / break at 2:00 to 2:40");
 	}
 	
 
@@ -127,27 +131,27 @@ public class Worker {
 		return name;
 	}
 
-	public static void setName(String name) {
-		Worker.name = name;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	
 
 
-	public static int getPhone() {
+	public  int getPhone() {
 		return phone;
 	}
 
-	public static  void setPhone(int phone) {
-		Worker.phone = phone;
+	public void setPhone(int phone) {
+		this.phone = phone;
 	}
 
-	public static String getAdder() {
+	public  String getAdder() {
 		return adder;
 	}
 
-	public static void setAdder(String adder) {
-		Worker.adder = adder;
+	public void setAdder(String adder) {
+		this.adder = adder;
 	}
 
 	
